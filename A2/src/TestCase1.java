10b22a72d62179d997ec1c08539f36ad3ba3f0f4
Farwa <Farwa@DESKTOP-06V5Q8Q.mshome.net>
2022-10-31 11:01:19 +0500

    First Commit

---
+++ A2/src/TestCase1.java
@@ -0,0 +1,13 @@
+package TestCase1;
+import org.openqa.selenium.webDriver;
+public class TestCase1 {
+WebDriver driver;
+	public void setUp()
+	{
+		
+    }
+	public void tearDown()
+	{
+		
+    }
+}
---
+++ A2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Farwa
+ *
+ */
+module A2 {
+}
