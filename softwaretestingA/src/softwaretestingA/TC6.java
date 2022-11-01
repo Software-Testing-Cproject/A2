f6583700e4932834f09ed63db1db28467af63702
Farwa Batool <81681241+farwabatool292@users.noreply.github.com>
2022-11-01 22:24:57 +0500

    Update TC6.java

--- softwaretestingA/src/softwaretestingA/TC6.java
+++ softwaretestingA/src/softwaretestingA/TC6.java
@@ -1,5 +1,31 @@
 package softwaretestingA;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class TC6 {
+	WebDriver driver;
+	@BeforeMethod
+	public void setUp()
+	{ 
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Farwa\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.get("http://localhost/product/chair");
+	driver.manage().window().maximize();
+
+	}
+	@Test
+	public void verify()
+	{
+
+	}
+		@AfterMethod
+		public void tearDown()
+		{ 
+			driver.quit();
+		}
 
 }
