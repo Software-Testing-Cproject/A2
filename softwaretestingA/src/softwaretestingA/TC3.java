61bcfb65d39e4a39656e085cba8243ee34755859
Farwa Batool <81681241+farwabatool292@users.noreply.github.com>
2022-11-05 14:21:43 +0500

    Updated verify function for Checkout page
    Test case: 
    Given I have added  products in the Shopping Cart When I click on check out from Home page Then It take me to check out page

--- softwaretestingA/src/softwaretestingA/TC3.java
+++ softwaretestingA/src/softwaretestingA/TC3.java
@@ -18,6 +18,27 @@ public class TC3 {
 	@Test
 	public void verify()
 	{
+		System.out.println("Test case");
+		System.out.println("Given I have added products in the Shopping Cart When I click on check out from Home page Then It take me to check out page");
+		driver.findElement(By.className("active")).click(); //ADD THE prdocut to cart
+		driver.findElement(By.className("icon-cart")).click(); //press the cart icon
+		 driver.findElement(By.name("Checkout")).click(); //using Selenium click button method, click on checkout button
+		System.out.println("Successful in proceeding to Checkout Page");
+		ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+		driver.switchTo().window(tabs.get(1));//jumping to next window
+		//driver control is switched to new tab or new window
+		    String newUrl = driver.getCurrentUrl();//getting url of new windo
+
+	    if(newUrl.equalsIgnoreCase("http://localhost/checkout"))//comparing the URL
+	    {
+	        System.out.println("Successful in proceeding to Checkout page ");	
+	        
+	    }
+	    else {
+	        System.out.println("Failed in proceeding to Checkout page");
+			
+	    }
+
 
 	}
 		@AfterMethod
