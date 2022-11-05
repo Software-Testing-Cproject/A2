ba0ad184d6a3e75237a854d5670cd6ad19a1cb04
JaveriaFAST <117253648+JaveriaFAST@users.noreply.github.com>
2022-11-05 15:44:44 +0500

    Update UI_TC1.java

--- ST_A2_UI_Testing/src/UITCpackage/UI_TC1.java
+++ ST_A2_UI_Testing/src/UITCpackage/UI_TC1.java
@@ -1,10 +1,12 @@
 package UITCpackage;
 import java.time.Duration;
+import java.util.List;
 
-import javax.swing.JButton;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,22 +18,41 @@ public class UI_TC1 {
 	@BeforeMethod
 	public void setUp()
 	{
-	System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver_win32\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
 	driver = new ChromeDriver();
-	driver.get("http://localhost/product/chair");
+	driver.get("http://localhost");
 	driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+   
 	}
 	@Test
-	public void verify()
+	public void verifydropdown()
 	{
-	String actualtitle=driver.getTitle();
-	String expectedtitle="Shopizer";
-	Assert.assertEquals(actualtitle,expectedtitle);
+
+		Actions s=new Actions(driver);
+
+		
+		WebElement check1= driver.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/div/div[1]/div[1]/span/i"));
+		s.moveToElement(check1);
+		s.click(check1);
+s.build().perform();
+		 
+	WebElement check=driver.findElement(By.xpath("/html/body/div[1]/header/div[1]/div/div/div[1]/div[1]/div/ul/li/button"));
+	
+	s.moveToElement(check);
+	s.click(check);
+
+	s.build();
+	driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(4));
+	s.perform();
+	
+
+	
+	
 	}
 	@AfterMethod
 	public void tearDown()
 	{
+		 
 	driver.quit();
 	}
 	
