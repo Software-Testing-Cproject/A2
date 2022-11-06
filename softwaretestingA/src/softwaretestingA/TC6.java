19797cd15221995c30afc27ce526d481df163b0a
Farwa Batool <81681241+farwabatool292@users.noreply.github.com>
2022-11-07 03:14:22 +0500

    updated

--- softwaretestingA/src/softwaretestingA/TC6.java
+++ softwaretestingA/src/softwaretestingA/TC6.java
@@ -1,31 +1,24 @@
 package softwaretestingA;
+import io.restassured.response.Response;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
 
 public class TC6 {
-	WebDriver driver;
-	@BeforeMethod
-	public void setUp()
-	{ 
-	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Farwa\\Downloads\\chromedriver_win32\\chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.get("http://localhost/product/chair");
-	driver.manage().window().maximize();
-
-	}
 	@Test
-	public void verify()
-	{
-
+public void test_delCustomer()//deleting the customer
+{
+		Response r=
+		given()
+		 .when()
+		  .get("https://rest-api.shopizer.com/api/v1/private/customer/123")//putting url which include ID
+		  .then()
+		  .statusCode(200)//if successful then code 200
+		  .log().body()
+	    .extract().response();
+		String jsonstr=r.asString();
+		Assert.assertEquals(jsonstr.contains("Data Saved Successfully"), true);//matching with the statement which we get after the customer del
 	}
-		@AfterMethod
-		public void tearDown()
-		{ 
-			driver.quit();
-		}
 
 }
