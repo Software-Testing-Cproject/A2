dc2df7ae5c0d8c869aa25ebc19c827d6bb7af767
HamnaArifeen123 <93051277+HamnaArifeen123@users.noreply.github.com>
2022-11-07 12:32:55 +0500

    delete:product(image)

--- Hamna_TC/src/TCPackage_1/TC_6.java
+++ Hamna_TC/src/TCPackage_1/TC_6.java
@@ -1,5 +1,30 @@
 package TCPackage_1;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
 public class TC_6 {
 
+	
+	@Test
+	   public void delete_product()//remove option value image
+	   {
+		Response rspn=
+			given()
+			
+			.when()
+			  .get("https://rest-api.shopizer.com/api/v1/private/product/option/value/345/image") 
+			.then()
+			
+			  .statusCode(200)
+			  .log().body()
+			  .extract().response();//for verifyion
+			  
+			  
+		String res_json=rspn.asString();
+		Assert.assertEquals(res_json.contains("Successfully"), true);
+	   }
 }
