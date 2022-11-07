68cca30db396c5b420e246e839734c971902f116
HamnaArifeen123 <93051277+HamnaArifeen123@users.noreply.github.com>
2022-11-07 12:32:07 +0500

    Post:product

--- Hamna_TC/src/TCPackage_1/TC_5.java
+++ Hamna_TC/src/TCPackage_1/TC_5.java
@@ -1,5 +1,48 @@
 package TCPackage_1;
 
-public class TC_5 {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.HashMap;
+
+public class TC_5 {
+	@Test
+   public void add_new_product()//create option
+   {
+		
+		HashMap data=new HashMap();
+		data.put("code", "097");
+		data.put("description", "cloth");
+		data.put( "friendlyUrl", "hjuu/jj");
+		data.put("highlights", "brand");
+		 data.put("id", 30);
+		 data.put( "keyWords", ")(*");
+		 data.put( "language", "English");
+		 data.put( "metaDescription", "None");
+		data.put(   "name", "Product_clot");
+		 data.put(   "title", "J.");
+		data.put(  "id", 02);
+		data.put(  "order", 04);
+		data.put(  "readOnly", true);
+		data.put( "type", "blue");
+		
+		
+		Response rspn=
+		given()
+		    .contentType("application/json")
+		   .body(data)
+		.when()
+		  .post("https://rest-api.shopizer.com/api/v1/private/product/option")
+		.then()
+		  .statusCode(200)
+		  .log().body()
+		  .extract().response();//to get the resopnse
+		
+		String res_json=rspn.asString();
+		Assert.assertEquals(res_json.contains("Successfully"), true);
+   }
 }
