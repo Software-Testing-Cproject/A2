4eaa1ac3c0d625b95d731e6c994e1aeab9d0da40
HamnaArifeen123 <93051277+HamnaArifeen123@users.noreply.github.com>
2022-11-07 12:31:30 +0500

    Get:product attribute

--- Hamna_TC/src/TCPackage_1/TC_4.java
+++ Hamna_TC/src/TCPackage_1/TC_4.java
@@ -1,6 +1,20 @@
 package TCPackage_1;
 
-public class TC_4 {
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
 
- 
+public class TC_4 {
+	@Test
+   public void Product_attribute()
+   {
+		//get the atrribute by checking the existing code
+		given()
+		
+		.when()
+		  .get("https://rest-api.shopizer.com/api/v1/private/product/option/unique") 
+		.then()
+		
+		  .statusCode(200);///ok for 200
+   }
 }
+
